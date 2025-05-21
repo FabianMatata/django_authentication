cbc709ac43545790a84279ed513bd2c40f260c39
FabianMatata <mfabian601@gmail.com>
2025-05-21 16:48:40 +0300

    django authentication

---
+++ ARCrest/views.py
@@ -0,0 +1,8 @@
+# views.py
+from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse("Hello, welcome to the home page!")
+
+def index(request):
+    return HttpResponse("hello wangari josephine")
